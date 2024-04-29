4f933ba54c1160da213f48b999db1f477869c19d
midhunbinoy66 <midhunbinoy508@gmail.com>
2024-04-29 16:27:27 +0530

    removed .env

--- frontEnd/src/app/helpers/jwt-token.ts
+++ frontEnd/src/app/helpers/jwt-token.ts
@@ -1,7 +1,6 @@
 // Function to check is passed jwt token is expired or not
 export function isTokenExpired (token: string): boolean {
     const payload = JSON.parse(atob(token.split('.')[1]))
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (payload?.exp) {
       const expireTime = payload.exp * 1000
       if (expireTime > Date.now()) return false
